767b5f97aa416a5c1709ec34cb6eb621ab972b23
Shaban-Eissa <shabaneissa767@gmail.com>
2025-10-15 17:26:54 +0300

    🎉 Initial Commit: Initialize Angular project with configuration files and dependencies

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from './components/dashboard.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [DashboardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
